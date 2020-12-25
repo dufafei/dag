a656252e731cdaf63ae6fbd962910dd05bd24d02
杜发飞 <dufafei@hikcreate.com>
2020-12-26 02:49:58 +0800

    提交项目

--- src/main/java/com/dataflow/frame/resolver/JsonCoder.java
+++ src/main/java/com/dataflow/frame/resolver/JsonCoder.java
@@ -1,4 +1,11 @@
 package com.dataflow.frame.resolver;
 
+import com.dataflow.frame.meta.FlowMeta;
+
 public interface JsonCoder {
+
+    FlowMeta decode(String xml) throws Exception;
+
+    String encode(FlowMeta meta) throws Exception;
+
 }
